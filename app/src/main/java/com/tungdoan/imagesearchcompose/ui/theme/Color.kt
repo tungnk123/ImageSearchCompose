28f9f794fae2fec70c8847195fdb53f42150a6ce
tungnk123 <doanthanhtungnk123@gmail.com>
2024-06-14 09:38:49 +0700

    Set up color and theme for project

--- app/src/main/java/com/tungdoan/imagesearchcompose/ui/theme/Color.kt
+++ app/src/main/java/com/tungdoan/imagesearchcompose/ui/theme/Color.kt
@@ -9,3 +9,8 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val Background = Color(0xFFEAFFD0)
+val Purple200 = Color(0xFFDFCCFB)
+val Purple400 = Color(0xFFD0BFFF)
+val Orange = Color(0xFFFCE38A)
